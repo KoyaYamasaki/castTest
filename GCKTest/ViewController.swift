0a789ed61bd4f2cb11067a0d48f155fdd8fa18e6
houdini217 <koya1@yamazakihiroshinari-no-MacBook-Pro.local>
2018-12-28 00:29:29 +0900

    first commit

---
+++ GCKTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GCKTest
+//
+//  Created by 山崎宏哉 on 2018/12/26.
+//  Copyright © 2018 山崎宏哉. All rights reserved.
+//
+
+import UIKit
+
+/// ViewController
+class ViewController: UIViewController {
+
+    /// viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+use_frameworks!
+
+platform :ios, '9.0'
+
+def target_pods
+  pod 'google-cast-sdk', '< 5.0', '>=4.3.3' 
+end
+
+target 'GCKTest' do
+  target_pods
+end
